4304bf9dc2a55a76cc365a484d1dec0d8245fb38
alexey_ignatov <alexey.ignatov.msu@gmail.com>
2020-02-27 03:23:08 +0300

    add gitignore

--- qteam_bot_server/simple_cards/models.py
+++ qteam_bot_server/simple_cards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+class Card(models.Model):
+    title = models.CharField(max_length=200)
+    card_text = models.CharField(max_length=2000)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
+
+
+
+
