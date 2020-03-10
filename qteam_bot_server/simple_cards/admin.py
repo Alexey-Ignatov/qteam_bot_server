b18985e6b9748bbfa4fdbcde5f18e9bd700b7ca0
alexey_ignatov <alexey.ignatov.msu@gmail.com>
2020-03-10 16:16:11 +0300

    cardset summary added

--- qteam_bot_server/simple_cards/admin.py
+++ qteam_bot_server/simple_cards/admin.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Card, CardLike, CardDislike,BotUser,BotUserToCardCategory, CardCategory,BookEveningEvent,CardDate,DateUserCardSet
 import json
-
+from .views import get_cards_ok_to_show
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_display= ('title', 'num_likes', 'num_dislikes', 'is_active', 'num_shows')
+    list_display= ('title', 'num_likes', 'num_dislikes',  'num_shows', 'is_available_now','is_active')
 
 
     def num_likes(self, obj):
@@ -22,6 +22,12 @@ class CardAdmin(admin.ModelAdmin):
     def num_shows(self, obj):
         sets = DateUserCardSet.objects.all()
         return len([True for el in sets if obj.id in json.loads(el.card_ids)])
+
+    def is_available_now(self, obj):
+        good_cards = get_cards_ok_to_show()
+        return obj in good_cards
+
+    is_available_now.boolean = True
     #fields = ('title', 'num_likes')
 
 
